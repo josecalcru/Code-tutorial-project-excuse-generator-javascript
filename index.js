9694f5c3ccf44cdffd3df5a35b908e229f6e48c0
josecalcru <josecalcru@gmail.com>
2021-02-06 00:46:02 +0000

    subida archivos

---
+++ index.js
@@ -0,0 +1,20 @@
+alert("Hola desde archivo js")
+console.log("Hola console log desde js")
+
+let nombre = "Andres"
+
+let estudiantes = ["Pablo", "Esther", "Laura", "Maca"]
+
+let edad = 12
+
+if (edad >= 18){
+
+    console.log("Ya puedes sacar registro")
+}
+else{
+    console.log("Te faltan un par de anhos para manejar")
+}
+
+console.log(estudiantes)
+
+console.log("Hola mi nombre es " + nombre)
